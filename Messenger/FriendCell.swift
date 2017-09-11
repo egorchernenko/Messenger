877e1d03d123eaf0fa7b300f6afb5df6d564332a
egorchernenko <lordo916@gmail.com>
2017-09-11 15:25:28 +0300

    add UI and set auto-layout programmaticlly;

---
+++ Messenger/FriendCell.swift
@@ -0,0 +1,114 @@
+//
+//  FriendCell.swift
+//  Messenger
+//
+//  Created by Egor on 11.09.17.
+//  Copyright © 2017 Egor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FriendCell: BaseCell{
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 34
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    let dividerLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.5, alpha: 0.5)
+        return view
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Friend Name"
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textAlignment = .left
+        return label
+    }()
+    
+    let messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Here you can see your friend's messageasdfsfasfsafsfsfasdfsdfsdfsad..."
+        label.textColor = UIColor.darkGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "12:05 pm"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .right
+        return label
+    }()
+    
+    let hasReadImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    override func setupView(){
+        
+        addSubview(profileImageView)
+        addSubview(dividerLineView)
+        
+        setupContainerView()
+        
+        profileImageView.image = #imageLiteral(resourceName: "zuckerberg")
+        hasReadImageView.image = #imageLiteral(resourceName: "zuckerberg")
+        
+        profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 16, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 68, heightConstant: 68)
+        
+        dividerLineView.anchor(nil, left: profileImageView.rightAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 1)
+    }
+    
+    //MARK: - Add friend name and message label
+    
+    private func setupContainerView(){
+        let containerView = UIView()
+        addSubview(containerView)
+        
+        containerView.anchor(self.topAnchor, left: profileImageView.rightAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(messageLabel)
+        containerView.addSubview(timeLabel)
+        containerView.addSubview(hasReadImageView)
+        
+        nameLabel.anchor(containerView.topAnchor, left: containerView.leftAnchor, bottom: nil, right: containerView.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        messageLabel.anchor(nameLabel.bottomAnchor, left: containerView.leftAnchor, bottom: nil, right: hasReadImageView.leftAnchor, topConstant: 4, leftConstant: 0, bottomConstant: 0, rightConstant: 4, widthConstant: 0, heightConstant: 0)
+        
+        timeLabel.anchor(containerView.topAnchor, left: nil, bottom: nil, right: containerView.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 8, widthConstant: 100, heightConstant: 0)
+        
+        hasReadImageView.anchor(nameLabel.bottomAnchor, left: nil, bottom: nil, right: containerView.rightAnchor, topConstant: 4, leftConstant: 0, bottomConstant: 0, rightConstant: 8, widthConstant: 20, heightConstant: 20)
+    }
+    
+}
+
+class BaseCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView(){
+        backgroundColor = .blue
+    }
+}
---
+++ Messenger/FriendsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Egor on 11.09.17.
+//  Copyright © 2017 Egor. All rights reserved.
+//
+
+import UIKit
+
+class FriendsViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    private let cellId = "cellId"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Recent"
+        
+        collectionView?.backgroundColor = .white
+        collectionView?.alwaysBounceVertical = true
+    
+        collectionView?.register(FriendCell.self, forCellWithReuseIdentifier: cellId)
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        return cell
+    }
+    
+    //MARK: - Flow layout 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+}
